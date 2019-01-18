637aa535420a794a11aa61de3ae9a5c31ce21d43
amavr <albert.mavrin@gmail.com>
2019-01-18 17:55:29 +0300

    яяя

--- hello.go
+++ hello.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// func main() {
+// 	fmt.Printf("hello, world 2\n")
+// }
 
 func main() {
-	fmt.Printf("hello, world 2\n")
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
+	// err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":5050", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "hello, world")
 }
